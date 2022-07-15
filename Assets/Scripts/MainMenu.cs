01e40d2ab5267e4dc41fead1f3721e0e721079f2
Shai Aharoni <saharoni@gmail.com>
2022-07-15 16:32:02 +0300

    Add menu functionallity

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,4 +10,10 @@ public class MainMenu : MonoBehaviour {
     {
         SceneManager.LoadScene(1);
     }
+
+    public void QuitSimulation()
+    {
+        Debug.Log("Quitting application");
+        Application.Quit();
+    }
 }
